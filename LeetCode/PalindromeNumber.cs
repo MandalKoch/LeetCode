4924827fbc767c4c5bec61533955a3cce30511d7
Andreas Koch <andreas.Koch2010@web.de>
2024-02-18 22:20:14 +0100

    Sovling  https://leetcode.com/problems/palindrome-number/

---
+++ LeetCode/PalindromeNumber.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode;
+
+/// <summary>
+///     https://leetcode.com/problems/palindrome-number/
+/// </summary>
+public static class PalindromeNumber
+{
+    public static bool IsPalindrome(int x)
+    {
+        if (x < 0) return false;
+        var input = x.ToString().ToCharArray();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (i > input.Length/2)
+            {
+                return true;
+            }
+            if (input[i] != input[input.Length - 1 - i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static bool IsPalindromV2(int x)
+    {
+        int r = 0, c = x;
+        while (c > 0)
+        {
+            r = r * 10 + c % 10;
+            c /= 10;
+        }
+        return r == x; 
+    }
+}
---
+++ LeetCodeTests/PalindromeNumberTests.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode;
+
+public class PalindromeNumberTests
+{
+    [Theory]
+    [InlineData(121, true)]
+    [InlineData(-121, false)]
+    [InlineData(10, false)]
+    [InlineData(0, true)]
+    public void PalindromeTests(int x, bool assert)
+    {
+        var answer = PalindromeNumber.IsPalindrome(x);
+        answer.Should().Be(assert);
+    }
+    [Theory]
+    [InlineData(121, true)]
+    [InlineData(-121, false)]
+    [InlineData(10, false)]
+    [InlineData(0, true)]
+    public void PalindromeV2Tests(int x, bool assert)
+    {
+        var answer = PalindromeNumber.IsPalindromV2(x);
+        answer.Should().Be(assert);
+    }
+}
